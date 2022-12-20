abacc205cf122e6c103ff436989f7593d1ad3d37
bhanu <udaibhanu2602@gmail.com>
2022-12-20 10:30:12 +0530

    Hoisted on Firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
 import Header from "./Components/Layout/Header";
 import Footer from "./Components/Layout/Footer";
-import React, { useEffect } from "react";
-import Auth from "./Components/Auth/Auth";
-import Mailbox from "./Components/page/Mail/Mailbox";
+import React, { useEffect,Suspense } from "react";
+//import Auth from "./Components/Auth/Auth";
+//import Mailbox from "./Components/page/Mail/Mailbox";
 import { Route, Switch } from "react-router-dom";
-import MessageInbox from "./Components/page/MessageInbox/MessageInbox";
+//import MessageInbox from "./Components/page/MessageInbox/MessageInbox";
 import { useDispatch, useSelector } from "react-redux";
 import { ActionCreater } from "./Components/store/store-actions";
 import { ActionForSentMail } from "./Components/store/store-actions";
-import SentBox from "./Components/page/SentBox/SentBox";
+//import SentBox from "./Components/page/SentBox/SentBox";
+import Loading from "./Components/Layout/UI/Loading";
+const Auth=React.lazy(()=>import("./Components/Auth/Auth"))
+const Mailbox=React.lazy(()=>import("./Components/page/Mail/Mailbox"))
+const MessageInbox=React.lazy(()=>import("./Components/page/MessageInbox/MessageInbox"))
+const SentBox=React.lazy(()=>import("./Components/page/SentBox/SentBox"))
+
 function App() {
   const dispatch = useDispatch();
   const userEmail = useSelector((state) => state.auth.MailBoxId);
@@ -17,6 +23,7 @@ function App() {
     let id = setInterval(() => {
       console.log("CALLING INTERVAL")
       dispatch(ActionCreater(userEmail));
+      dispatch(ActionForSentMail(userEmail))
     }, 2000);
     return(()=>clearInterval(id))
   }, []);
@@ -25,6 +32,7 @@ function App() {
   return (
     <React.Fragment>
       <Header />
+      <Suspense fallback={<Loading>Loading...</Loading>} >
       <Switch>
         <Route path="/" exact>
           <Auth />
@@ -40,6 +48,7 @@ function App() {
           <SentBox />
         </Route>
       </Switch>
+      </Suspense>
       <Footer />
     </React.Fragment>
   );
